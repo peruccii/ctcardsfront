90dc910d52188d87de4c85dd5646ae325ff77b4d
peruccii <peruccii2917@hotmail.com>
2024-12-26 22:38:25 -0300

    refatorate all this shit code : D

--- src/interfaces/form_values.ts
+++ src/interfaces/form_values.ts
@@ -1,8 +1,13 @@
+import { InvitePlan } from "@/app/enums/invite_plan";
+import { InviteType } from "@/app/enums/invite_type";
+
 export interface FormValues {
   title: string;
   sub_title: string;
   message: string;
   email: string;
-  url_music: string;
-  date: Date
+  url_music: string | null;
+  date: Date;
+  invite_type: InviteType
+  invite_plan: InvitePlan
 }
