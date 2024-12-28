dc470ccc7152541b388b607178be83f6eb946725
peruccii <peruccii2917@hotmail.com>
2024-12-28 19:15:09 -0300

    feliz ano novo

--- src/app/page/[id]/cardpage.tsx
+++ src/app/page/[id]/cardpage.tsx
@@ -1,3 +1,19 @@
-const CardPage = () => {
-  return <div></div>;
+import { InviteUseCases } from '@/app/api/invite-use-cases';
+import { InviteRepositoy } from '@/app/invite-repository';
+
+interface searchParamsProps {
+  searchParams: {
+    id: string;
+  };
+}
+
+export const CardPage = async (
+  { searchParams }: searchParamsProps,
+  repo: InviteRepositoy = new InviteUseCases(),
+) => {
+  const id = searchParams.id;
+
+  const data = await repo.get(id);
+
+  return <h1>{data.message}</h1>;
 };
