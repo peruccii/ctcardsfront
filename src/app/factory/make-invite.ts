a77ce0dfdb9bc96cc654c1c090104ab05c563513
peruccii <peruccii2917@hotmail.com>
2025-01-02 22:18:01 -0300

    refatorate

--- src/app/factory/make-invite.ts
+++ src/app/factory/make-invite.ts
@@ -1,18 +1,35 @@
+import { IFieldObject } from '@/components/MessageContext';
+import { getColorTypeInvite } from '@/components/TypeInviteColor';
 import { FormValues } from '@/interfaces/form_values';
 
-export function makeInvite(value: FormValues) {
+function MakeInvite(
+  value: FormValues,
+  searchParams: URLSearchParams,
+  data: IFieldObject,
+) {
   const formdata = new FormData();
+  const dateNow = new Date().toISOString();
+  const cardColor =
+    searchParams.get('card_color') ??
+    getColorTypeInvite(searchParams.get('type')!);
 
-  formdata.append('title', value.title);
-  formdata.append('sub_title', value.sub_title);
-  formdata.append('message', value.message);
-  formdata.append('email', value.email);
-  formdata.append('date', value.date.toISOString());
-  formdata.append('invite_type', value.invite_type);
-  formdata.append('invite_plan', value.invite_plan);
+  formdata.append('title', data.title ?? '');
+  formdata.append('sub_title', data.title ?? '');
+  formdata.append('payment_method', localStorage.getItem('payment_method')!);
+  formdata.append('message', data.message ?? '');
+  formdata.append('email', data.email ?? '');
+  formdata.append('card_color', cardColor!);
+  formdata.append('bg_color', searchParams.get('bg') ?? '#FFFFFF');
+  formdata.append('names', searchParams.get('names')!);
+  formdata.append('date', searchParams.get('date')!);
+  formdata.append('createdAt', dateNow);
+  formdata.append('invite_type', searchParams.get('type')!);
+  formdata.append('invite_plan', searchParams.get('plan')!);
   if (value.url_music && value.url_music.length)
-    formdata.append('url_music', value.url_music);
-  value.images.forEach((image) => formdata.append('imageUrls', image));
+    formdata.append('url_music', searchParams.get('url_music')!);
+  value.imageUrls.forEach((image) => formdata.append('imageUrls', image));
 
   return formdata;
 }
+
+export default MakeInvite;
