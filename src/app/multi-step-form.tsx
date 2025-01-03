21aeee75ac05b8430ed408807d8d61af318c5312
peruccii <peruccii2917@hotmail.com>
2025-01-02 22:18:22 -0300

    refatorate

--- src/app/multi-step-form.tsx
+++ src/app/multi-step-form.tsx
@@ -2,31 +2,34 @@
 'use client';
 
 import { useState } from 'react';
-import { renderFormStep } from '@/components/RenderFormStep';
+import RenderFormStep from '@/components/RenderFormStep';
 import { useForm } from '@tanstack/react-form';
 import { steps, stepsTitleContent } from './steps/steps';
 import { Card } from '@/components/ui/card';
 import { BorderBeam } from '@/components/ui/border-beam';
 import ProgressBar from '@/components/ProgressBar';
 import { FormValues } from '@/interfaces/form_values';
-import { renderCard } from '@/components/RenderCard';
+import RenderCard from '@/components/RenderCard';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { ResponseCheckout } from '@/interfaces/response_checkout';
-import { makeInvite } from './factory/make-invite';
+// import { ResponseCheckout } from '@/interfaces/response_checkout';
 import { InviteUseCases } from './api/invite-use-cases';
+import useUploadPhotos from '@/components/useUploadPhotos';
+import MakeInvite from './factory/make-invite';
+import { useMessage } from '@/components/MessageContext';
 
 export default function MultiStepForm() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
+  const { data } = useMessage();
   const { replace, push } = useRouter();
   const params = new URLSearchParams(searchParams);
+  const { files, setFiles } = useUploadPhotos();
 
   const [currentStepContent, setCurrentStepContent] = useState(
     stepsTitleContent[Number(searchParams.get('step'))],
   );
 
-  const handleNext = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
+  const handleNext = () => {
     if (Number(searchParams.get('step')) < steps.length - 1) {
       params.set('step', String(Number(searchParams.get('step')) + 1));
       replace(`${pathname}?${params.toString()}`, { scroll: false });
@@ -49,35 +52,48 @@ export default function MultiStepForm() {
 
   const form = useForm<FormValues>({
     onSubmit: async ({ value }) => {
-      const formdata = makeInvite(value);
-
+      const formdata = MakeInvite(value, searchParams, data);
+      console.log(value);
       const invite = new InviteUseCases();
 
-      const data = await invite.create(formdata);
+      const response = await invite.create(formdata);
 
-      const response: ResponseCheckout = await data.json();
-
-      if (data.status === 200) push(response.urlStripe);
+      console.log('response', response);
+      // if (dataR.status === 201) push(response.url);
     },
   });
-
   return (
-    <div className="min-h-screen flex items-center justify-center p-8">
-      <Card className="w-full max-w-[1400px] shadow-lg overflow-hidden border-2 border-orange-100 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 backdrop-saturate-50 backdrop-contrast-125">
+    <div className="min-h-screen flex items-center justify-center ">
+      <Card className="w-full max-w-[1700px] h-full  shadow-lg overflow-hidden border-2 border-orange-100 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 backdrop-saturate-50 backdrop-contrast-125">
         <BorderBeam borderWidth={3} />
         <div className="flex flex-col md:flex-row m-6 rounded-lg bg-white">
-          <div className="w-full md:w-1/2 p-12">
+          <div className="w-full md:w-1/2 sm:p-12 p-2  items-center">
             <ProgressBar
               steps={steps}
               currentStep={Number(searchParams.get('step'))}
             />
 
-            <h2 className="text-2xl font-bold mt-8 mb-4">
+            <h2 className="sm:text-2xl text-sm  sm:justify-start justify-center flex  font-bold mt-8 mb-4">
               {currentStepContent}
             </h2>
-            <form className="space-y-8 h-auto">
-              {renderFormStep(Number(searchParams.get('step')), form)}
-              <div className="items-end justify-end bottom-4 right-4 flex gap-4">
+            <form
+              className="space-y-8 h-auto"
+              onSubmit={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                void form.handleSubmit();
+              }}
+            >
+              {RenderFormStep(
+                Number(searchParams.get('step')),
+                form,
+                params,
+                pathname,
+                replace,
+                setFiles,
+                files,
+              )}
+              <div className="sm:items-end items-center sm:justify-end justify-center bottom-4 right-4 flex gap-4">
                 <button
                   type="button"
                   onClick={handlePrev}
@@ -97,19 +113,18 @@ export default function MultiStepForm() {
                           : 'bg-gradient-to-r from-orange-600 via-orange-500 to-orange-400 shadow-lg shadow-orange-500/50 hover:bg-gradient-to-l'
                       }`}
                       type="button"
-                      onClick={(e) => {
-                        if (canSubmit) handleNext(e);
-                      }}
+                      onClick={handleNext}
                       disabled={!canSubmit}
                     >
                       Prosseguir
                     </button>
                   )}
                 />
+                <button type="submit">submit</button>
               </div>
             </form>
           </div>
-          {renderCard(form.state.values.title)}
+          {RenderCard(params.get('type')!, params, files)}
         </div>
       </Card>
     </div>
