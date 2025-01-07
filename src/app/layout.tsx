8cf0a6df203871cb810442cda6004b8b0715c160
peruccii <peruccii2917@hotmail.com>
2025-01-07 20:50:33 -0300

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/cute_cards_icon.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
